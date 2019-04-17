b3b0235d38b49b1d868d02a9ff0ea6fdf4a1ce83
Timothy Prestby <tjprestby@gmail.com>
2019-04-17 12:33:12 -0500

    Added basemap

--- js/main.js
+++ js/main.js
@@ -3,22 +3,25 @@
 function createMap(){
 	var map = L.map('map',{
 		//Sets the longitude and latitude of where the map center is
-			center: [51, 12],
-			zoom: 5,
+			center: [37,-97],
+			zoom: 4,
 		});
 		//Add OSM baselayer
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { 
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-				maxZoom: 6,
-				minZoom:5,
-				//sets the max zoom level of the map
-				id: 'mapbox.dark',
-				//the id of the map to copy
-				accessToken: 'pk.eyJ1IjoicHJlc3RpbW9qIiwiYSI6ImNqczNmYWE2bzJmNTYzeW8zOXNlMnVpOGwifQ.OrEG7gIMeP3N3sMaNY3EGw'
-			 //DEFINE THE TILE LAYER THAT WILL BE USED//
-			}).addTo(map)   
+		L.tileLayer.provider('CartoDB.DarkMatterNoLabels').addTo(map);
+		//Remove tile outlines
+		(function(){
+			var originalInitTile = L.GridLayer.prototype._initTile
+			L.GridLayer.include({
+				_initTile: function (tile) {
+					originalInitTile.call(this, tile);
 		
-
+					var tileSize = this.getTileSize();
+		
+					tile.style.width = tileSize.x + .5 + 'px';
+					tile.style.height = tileSize.y + 1 + 'px';
+				}
+			});
+		})()
 }
 
 $(document).ready(createMap);
