af640f78ddda1ac81a866494135ad5001c6c5790
jbrmarks <jbrmarks@gmail.com>
2019-04-17 13:09:12 -0500

    Working on slider
    Added info-pane to left side of body

--- js/main.js
+++ js/main.js
@@ -22,6 +22,29 @@ function createMap(){
 				}
 			});
 		})()
+    
+    // Create the sequence slider
+    createTimeline(map);
+}
+
+// A function to create the sequence controls for the interactive timeline
+function createTimeline(map){
+
+
+
+    // Append controls to container
+    $('#info-pane').append('<span id = timelineControls></span>');
+
+    
+    // Create range input element (slider)
+    $('#timelineControls').html('<input class = "timeline-slider" orientation = "vertical" id = "timelineSlider" type = "range" min = "1776" max = "2019" step="1" value = "1776">');
+    
+    // Add event listeners for slider
+    $('.timeline-slider').on('input', function(){
+        // TODO: change visible map
+        // TODO: change context pane if passing major event
+        
+    })
 }
 
 $(document).ready(createMap);
